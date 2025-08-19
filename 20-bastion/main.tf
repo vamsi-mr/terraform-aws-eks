b10d8ceb46bdf1b236108d125d1b8d617ec45a6d
Priyanka <priyanka.rel6@gmail.com>
2025-08-19 20:00:30 +0530

    added aws-eks iam instance profile

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id              = local.public_subnet_id
-  iam_instance_profile = "TerraformAdmin"  ## no need to configure aws again in bastion
+  iam_instance_profile = "AWS-EKS"  ## no need to configure aws again in bastion
 
 # need more for terraform
 root_block_device {
