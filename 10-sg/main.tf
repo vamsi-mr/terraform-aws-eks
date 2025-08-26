a8d5cbd160d97ffb4577e472fbb3595b0c7d2e12
Priyanka <priyanka.rel6@gmail.com>
2025-08-26 09:27:45 +0530

    modified sg to allow pod to pod communication within vpc

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -197,5 +197,5 @@ resource "aws_security_group_rule" "eks_node_vpc" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["10.0.0.0/16"]
-  security_group_id = module.vpn.sg_id
+  security_group_id = module.eks_node.sg_id
 }
